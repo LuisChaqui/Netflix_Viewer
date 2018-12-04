78c55657a603f7099e1e4a8dd97c71d2ac4cb1cb
LuisChaqui <luigi92pm@gmail.com>
2018-12-04 16:54:54 -0500

    Interface

---
+++ src/com/lchaqui/vista/Aplicacion.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.lchaqui.vista;
+
+import com.lchaqui.controlador.Coordinador;
+
+/**
+ *
+ * @author PC6-LAB2
+ */
+public class Aplicacion {
+    public static void main(String[] args) {
+        Coordinador.showMenu();
+        //Magazine magazine = new Magazine();
+    }
+    
+}
